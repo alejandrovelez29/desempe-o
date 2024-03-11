569c0c0e799559769d2214c1e7a0dfc66ca9c27f
alejandrovelez29 <161092784+alejandrovelez29@users.noreply.github.com>
2024-03-11 18:59:52 -0500

    Add files via upload

---
+++ desempeño 3.py
@@ -0,0 +1,18 @@
+# Ejercicio 01
+
+a = (5 + 5) * 5
+b = 5 + (5 * 5)
+c = 5 % 2
+d = (5 - (6 / 3)) * 2
+
+print(f"{a}, {b}, {c}, {d}")
+
+
+# Ejercicio 02
+
+a = 7
+b = 3
+c = (a != b)
+d = (a==b)
+
+print(f"{c}, {d}, {a > b}, {(a+b) < 2}, {(a > 7 and b == 3)}, {a > 7 or b == 3}, {a < 7 or b != 3}")
